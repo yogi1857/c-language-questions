06cfe9f0e55a8d030051aaeac9b6d76ea36e3b1d
yogi1857 <90835257+yogi1857@users.noreply.github.com>
2021-09-23 17:16:13 +0530

    multiplication of two numbers

---
+++ question 2.c
@@ -0,0 +1,12 @@
+// program to mutiply two float numbers ?
+
+#include <stdio.h>
+
+int main()
+{
+ float a,b;
+ scanf("%f %f",&a,&b);
+ float mutiply = a*b;
+ printf("the multiplication of a and b is %.2f",mutiply);
+    return 0;
+}
