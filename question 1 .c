7ac0b9539b823cb813f18b2ff961e9ac99cb12f9
yogi1857 <90835257+yogi1857@users.noreply.github.com>
2021-09-23 17:15:14 +0530

    question 1
    sum of two numbers

---
+++ question 1 .c
@@ -0,0 +1,13 @@
+//creating a c program to add two integers ? 
+
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    scanf(" %d %d",&a,&b);
+    int sum = a+b ;
+    printf(" the sum of a and b is %d",sum);
+
+    return 0;
+}
