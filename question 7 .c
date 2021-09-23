7916dfba954496697762c194a6dc769b2e9d80c3
yogi1857 <90835257+yogi1857@users.noreply.github.com>
2021-09-23 17:25:54 +0530

    Printing months name recommanded to input number...

---
+++ question 7 .c
@@ -0,0 +1,57 @@
+// uses of switch
+
+#include <stdio.h>
+
+int main()
+{
+    int number ;
+    printf("please enter the number :");
+    scanf("%d",&number);
+    
+    switch(number){
+        case 1:
+        printf("January");
+        break;
+        case 2:
+        printf("Febuary");
+        break;
+        case 3:
+        printf("March");
+        break;
+        case 4:
+        printf("April");
+        break;
+        case 5:
+        printf("May");
+        break;
+        case 6:
+        printf("June");
+        break;
+        case 7:
+        printf("July");
+        break;
+        case 8:
+        printf("August");
+        break;
+        case 9:
+        printf("September");
+        break;
+        case 10:
+        printf("October");
+        break;
+        case 11:
+        printf("November");
+        break;
+        case 12:
+        printf("December");
+        break;
+        default :
+        printf("invalid input");
+        break;
+        
+        
+        
+    }
+
+    return 0;
+}
